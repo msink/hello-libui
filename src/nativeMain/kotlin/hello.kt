a69aea60befde2279446c9d54d4138b0367146c7
Mike Sinkovsky <msink@permonline.ru>
2018-11-26 14:59:33 +0500

    first commit

---
+++ src/nativeMain/kotlin/hello.kt
@@ -0,0 +1,25 @@
+import libui.ktx.*
+
+fun main() = appWindow(
+    title = "Hello",
+    width = 320,
+    height = 240
+) {
+    vbox {
+        lateinit var scroll: TextArea
+
+        button("libui говорит: click me!") {
+            action {
+                scroll.append("""
+                    |Hello, World!  Ciao, mondo!
+                    |Привет, мир!  你好，世界！
+                    |
+                    |""".trimMargin())
+            }
+        }
+        scroll = textarea {
+            readonly = true
+            stretchy = true
+        }
+    }
+}
