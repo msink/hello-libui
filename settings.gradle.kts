9e3cfe776967edf7fa1c4f72e4bd79d6e16e1c90
Mike Sinkovsky <msink@permonline.ru>
2019-01-25 13:02:41 +0500

    cleanup

--- settings.gradle.kts
+++ settings.gradle.kts
@@ -1,17 +1,1 @@
-pluginManagement {
-    resolutionStrategy {
-        eachPlugin {
-            if (requested.id.id == "kotlin-multiplatform") {
-                useModule("org.jetbrains.kotlin:kotlin-gradle-plugin:${requested.version}")
-            }
-        }
-    }
-
-    repositories {
-        maven { url = uri("https://dl.bintray.com/kotlin/kotlin-eap") }
-        mavenCentral()
-        maven { url = uri("https://plugins.gradle.org/m2/") }
-    }
-}
-
 enableFeaturePreview("GRADLE_METADATA")
