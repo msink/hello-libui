ca816a45aad00e14928f51915bb3438d87a292e2
Mike Sinkovsky <msink@permonline.ru>
2019-01-31 18:02:08 +0500

    cleanup

--- build.gradle.kts
+++ build.gradle.kts
@@ -29,9 +29,9 @@ kotlin {
 
 fun org.jetbrains.kotlin.gradle.plugin.mpp.Executable.windowsResources(rcFileName: String) {
     val rcFile = file("${compilation.defaultSourceSet.resources.sourceDirectories.asPath}/$rcFileName")
-    val resFile = file("$buildDir/windowsResources/${target.getName()}/${buildType.getName()}/${rcFileName.substringBefore(".rc")}.res")
+    val resFile = file("$buildDir/windowsResources/${target.getName()}/${buildType.getName()}/$baseName.res")
 
-    val windresTask = tasks.create<Exec>("compileWindowsResources${buildType.getName().capitalize()}${target.getName().capitalize()}") {
+    val windresTask = tasks.create<Exec>("windres${buildType.getName().capitalize()}${target.getName().capitalize()}") {
         val konanUserDir = System.getenv("KONAN_DATA_DIR") ?: "${System.getProperty("user.home")}/.konan"
         val konanLlvmDir = "$konanUserDir/dependencies/msys2-mingw-w64-x86_64-gcc-7.3.0-clang-llvm-lld-6.0.1/bin"
 
