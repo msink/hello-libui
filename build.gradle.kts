dd429b113257b8876369c0897ffad048ce501d71
Mike Sinkovsky <msink@permonline.ru>
2019-01-31 16:27:18 +0500

    cleanup

--- build.gradle.kts
+++ build.gradle.kts
@@ -32,21 +32,19 @@ kotlin {
 }
 
 fun org.jetbrains.kotlin.gradle.plugin.mpp.Executable.windowsResources(rcFileName: String) {
-    val suffix = "${buildType.getName().capitalize()}${target.getName().capitalize()}"
-    val resourcesDir = "$projectDir/src/${target.name}Main/resources"
-    val resFile = file("$buildDir/windowsResources$suffix/${rcFileName.substringBefore(".rc")}.res")
+    val rcFile = file("${compilation.defaultSourceSet.resources.sourceDirectories.asPath}/$rcFileName")
+    val resFile = file("$buildDir/windowsResources/${target.getName()}/${buildType.getName()}/${rcFileName.substringBefore(".rc")}.res")
 
-    val windresTask = tasks.create("compileWindowsResources$suffix", Exec::class) {
+    val windresTask = tasks.create<Exec>("compileWindowsResources${buildType.getName().capitalize()}${target.getName().capitalize()}") {
         val konanUserDir = System.getenv("KONAN_DATA_DIR") ?: "${System.getProperty("user.home")}/.konan"
         val konanLlvmDir = "$konanUserDir/dependencies/msys2-mingw-w64-x86_64-gcc-7.3.0-clang-llvm-lld-6.0.1/bin"
-        val rcFile = file("$resourcesDir/$rcFileName")
 
         inputs.file(rcFile)
         outputs.file(resFile)
         commandLine("$konanLlvmDir/windres", rcFile, "-D_${buildType.name}", "-O", "coff", "-o", resFile)
         environment("PATH", "$konanLlvmDir;${System.getenv("PATH")}")
 
-        dependsOn(tasks.named("compileKotlin${target.getName().capitalize()}"))
+        dependsOn(compilation.compileKotlinTask)
     }
 
     linkTask.dependsOn(windresTask)
