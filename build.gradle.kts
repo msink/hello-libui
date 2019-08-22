fce4fd58664cce2eff3183eefbca1bfea0e46ed0
Mike Sinkovsky <msink@permonline.ru>
2019-08-22 16:47:49 +0500

    Kotlin 1.3.50 and Gradle 5.6

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,5 +1,5 @@
 plugins {
-    kotlin("multiplatform") version "1.3.41"
+    kotlin("multiplatform") version "1.3.50"
 }
 
 repositories {
@@ -22,7 +22,7 @@ kotlin {
     }
     val libuiMain by sourceSets.getting {
         dependencies {
-            implementation("com.github.msink:libui:0.1.4")
+            implementation("com.github.msink:libui:0.1.5")
         }
     }
 }
